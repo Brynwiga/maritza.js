7255c4aae597db752357a5440c1945e79c6b73ba
Brynwiga <36052179+Brynwiga@users.noreply.github.com>
2019-11-16 05:56:03 -0700

    Create run.js
    This document runs any code written in maritza syntax on the webpage.

---
+++ run.js
@@ -0,0 +1,13 @@
+/*
+
+    (c) Aurelian University
+    
+    JavaScript implementation of the mar.js file
+
+*/
+
+window.onload = function(){
+    const main = new Program();
+    main.createApplications();
+    main.run();
+}
